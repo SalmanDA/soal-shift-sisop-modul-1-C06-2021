36a08b4b7c3432eea589d01051b41f84300b8cea
diladil <diladil809@gmail.com>
2021-04-04 20:55:50 +0700

    Revisi nomor 3

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -3,28 +3,29 @@
 #looping
 a=0
 
-declare -a array_compare[23]
+## Looping download
 while [ $a -lt 23 ]
 do
 	let b=$a+1
-	wget -o Foto.log -O /home/iamfadilahmad/uwu/Koleksi_$b 'http://loremflickr.com/320/240/kitten'
-	array_compare[$a]=$(awk 'NR==10{print $3}' Foto.log)
-	echo ${array_compare[a]}
+	wget -q -a Foto.log -O /home/iamfadilahmad/uwu/Koleksi_$b 'http://loremflickr.com/320/240/kitten'
 
-	#if [ $a -gt 0 ]
-	#then	let c=$a-1
-	#	while [ $c -ge 0 ]
-	#	do	if [ "${array_compare[$a]}" = "${array_compare[$c]}" ];
-	#		then	rm -v "Koleksi_b"
-	#		echo $c
-	#		echo $a
-	#		echo $b
-	#		let b=$b-1
-	#		fi
-	#	done
-	#fi 
+	##Collecting necessary hash value to compare
+	test[$a]="$(md5sum Koleksi_$b | awk '{print $1;}')"
+
+	##Comparing
+	for(( c=a-1; c>=0; c=c-1 ))
+	do
+		if [[ "${test[$a]}" == "${test[$c]}" ]];
+		then
+		rm Koleksi_$b
+		b=$((b-1))
+		fi
+	done
+
+	## increase counter
 	a=$((a+1))
 done
+
 
 
 
