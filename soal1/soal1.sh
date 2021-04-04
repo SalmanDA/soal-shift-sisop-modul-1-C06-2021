5ffde0f0faacee4d354a156b57ddd260f1144f12
ishaqadhel <ishaq.adhel@gmail.com>
2021-04-04 21:25:27 +0700

    upload img number 1

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -15,8 +15,8 @@
 
 #NOMOR 1B
 
-#grep "ERROR" $location/syslog.log
-#echo "Count Error: "; grep "ERROR" $location/syslog.log | grep -i -c "ERROR"
+#grep "ERROR" syslog.log
+#echo "Count Error: "; grep "ERROR" syslog.log | grep -i -c "ERROR"
 
 #End of NOMOR 1B
 
@@ -28,20 +28,20 @@
 
 #NOMOR 1D
 
-echo "Error,Count" >> error_message.csv
-i="0";
-for Line in `grep "ERROR" syslog.log | sed 's/.*ERROR \(.*\) (.*/\1,/g' | sort | uniq -c | sort -nr`
-do
-	if [[ "$Line" =~ [0-9] ]];
-	then
-		i=$Line
-	elif [[ "$Line" == *","* ]];
-	then
-		echo "${Line}${i}"
-	else
-		echo -n "${Line} " 
-	fi
-done >> error_message.csv
+#echo "Error,Count" >> error_message.csv
+#i="0";
+#for Line in `grep "ERROR" syslog.log | sed 's/.*ERROR \(.*\) (.*/\1,/g' | sort | uniq -c | sort -nr`
+#do
+#	if [[ "$Line" =~ [0-9] ]];
+#	then
+#		i=$Line
+#	elif [[ "$Line" == *","* ]];
+#	then
+#		echo "${Line}${i}"
+#	else
+#		echo -n "${Line} " 
+#	fi
+#done >> error_message.csv
 
 #End of NOMOR 1D
 
@@ -74,7 +74,7 @@ do
 		do
 			if [[ "$key"  == "$uLine" ]];
 			then
-               			echo "${uLine},$(( ${x}-${userError[${key}]} )),${userError[${key}]}"
+              			echo "${uLine},$(( ${x}-${userError[${key}]} )),${userError[${key}]}"
 				errorCount=1;
 				break;
 			fi
